4cc1d6bc9bdaf5388d995518def222e25a7eb5fd
Bill Peterman <peterman.73@osu.edu>
2024-02-27 15:24:24 -0500

    cdpop function fix

--- R/cdpop_setup.R
+++ R/cdpop_setup.R
@@ -11,12 +11,12 @@
 
 cdpop_setup <- function(){
   if(dir.exists(paste0(tempdir(),"\\cdpop"))){
-    to <- normalizePath(paste0(tempdir(),"\\cdpop"))
+    to <- normalizePath(paste0(tempdir(),"\\cdpop\\"))
 
   } else {
     from <- system.file("cdpop", package="CDPOPr")
     dir.create(paste0(tempdir(),"\\cdpop"), recursive = T, showWarnings = F)
-    to <- normalizePath(paste0(tempdir(),"\\cdpop"))
+    to <- normalizePath(paste0(tempdir(),"\\cdpop\\"))
 
     file.copy(list.files(from,
                          # recursive = TRUE,
